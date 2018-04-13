10570d5b53505c16ef9c49f14a47fea60e54d063
Pernfuß, Jörg <joerg.pernfuss@1und1.de>
2018-04-13 13:56:51 +0200

    stmt: start internal sql statement library

---
+++ internal/eye.stmt/name.go
@@ -0,0 +1,18 @@
+/*-
+ * Copyright (c) 2018, 1&1 Internet SE
+ * All rights reserved
+ *
+ * Use of this source code is governed by a 2-clause BSD license
+ * that can be found in the LICENSE file.
+ */
+
+// Package stmt provides SQL statement string constants for EYE
+package stmt // import "github.com/mjolnir42/eye/internal/eye.stmt"
+
+var m = make(map[string]string)
+
+func Name(statement string) string {
+	return m[statement]
+}
+
+// vim: ts=4 sw=4 sts=4 noet fenc=utf-8 ffs=unix
---
+++ internal/eye.stmt/registration.go
@@ -0,0 +1,44 @@
+/*
+ * Copyright (c) 2018, 1&1 Internet SE
+ * All rights reserved
+ *
+ * Use of this source code is governed by a 2-clause BSD license
+ * that can be found in the LICENSE file.
+ */
+
+package stmt // import "github.com/mjolnir42/eye/internal/eye.stmt"
+
+const (
+	RegistryStatements = ``
+
+	RegistryAdd = `
+CREATE TABLE IF NOT EXISTS eye.registry (
+  registrationID          uuid            PRIMARY KEY,
+  application             varchar(128)    NOT NULL,
+  address                 inet            NOT NULL,
+  port                    numeric(5,0)    NOT NULL CONSTRAINT valid_port CHECK ( port > 0 AND port < 65536 ),
+  database                numeric(5,0)    NOT NULL CINSTRAINT valid_db CHECK ( database >= 0 ),
+  registeredAt            timestamptz(3)  NOT NULL DEFAULT NOW(),
+  CONSTRAINT registeredAt_utc CHECK( EXTRACT( TIMEZONE FROM registeredAt ) = '0' )
+);`
+
+	RegistryDel = `
+DELETE FROM eye.registry
+WHERE  registrationID = $1::uuid;`
+
+	RegistrySearch = `
+SELECT registrationID
+FROM   eye.registry
+WHERE  application = $1::varchar
+  AND  address = $2::inet
+  AND  port = $3::numeric
+  AND  database = $4::numeric;`
+)
+
+func init() {
+	m[RegistryAdd] = `RegistryAdd`
+	m[RegistryDel] = `RegistryDel`
+	m[RegistrySearch] = `RegistrySearch`
+}
+
+// vim: ts=4 sw=4 sts=4 noet fenc=utf-8 ffs=unix

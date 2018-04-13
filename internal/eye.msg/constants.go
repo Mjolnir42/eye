ee55f55f0c1941de27716738591c2fb45825d301
Pernfuß, Jörg <joerg.pernfuss@1und1.de>
2018-04-13 15:34:16 +0200

    msg: update constants

--- internal/eye.msg/constants.go
+++ internal/eye.msg/constants.go
@@ -8,12 +8,35 @@
 
 package msg // import "github.com/mjolnir42/eye/internal/eye.msg"
 
+// Privileged access permission categories
+const (
+	CategoryOmnipotence = `omnipotence`
+	CategorySystem      = `system`
+)
+
 // Section Supervisor handles AAA requests outside the permission model
 const (
 	SectionSupervisor  = `supervisor`
 	ActionAuthenticate = `authenticate`
 	TaskBasicAuth      = `basic-auth`
 	VerdictOK          = 200
+)
+
+// Sections in category configuration are for action with configurations
+// as scope
+const (
+	CategoryConfiguration = `configuration`
+	SectionConfiguration  = `configuration`
+)
+
+// Actions for the various permission sections
+const (
+	ActionLookup = `lookup`
+)
+
+// Result codes
+const (
+	ResultOK = 200
 )
 
 // vim: ts=4 sw=4 sts=4 noet fenc=utf-8 ffs=unix

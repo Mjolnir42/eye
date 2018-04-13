5081e975ba1438f156453f530cab0e18db319294
Pernfuß, Jörg <joerg.pernfuss@1und1.de>
2018-04-13 15:42:32 +0200

    proto: add protocol for API version v2
    Adds new structs for API version v2 that are modelled after
    the SOMA protocol library structs, using a common request and
    result struct for client-server messaging.

---
+++ lib/eye.proto/v2__configuration.go
@@ -0,0 +1,48 @@
+/*-
+ * Copyright © 2018, 1&1 Internet SE
+ * All rights reserved.
+ *
+ * Use of this source code is governed by a 2-clause BSD license
+ * that can be found in the LICENSE file.
+ */
+
+package proto // import "github.com/mjolnir42/eye/lib/eye.proto"
+
+// Configuration holds the monitoring profile definition for a check
+// that has to be performed
+type Configuration struct {
+	ConfigurationID string      `json:"configurationID"`
+	Metric          string      `json:"metric"`
+	HostID          uint64      `json:"hostID,string"`
+	Tags            []string    `json:"tags,omitempty"`
+	Oncall          string      `json:"oncall"`
+	Interval        uint64      `json:"interval"`
+	Metadata        MetaData    `json:"metadata"`
+	Thresholds      []Threshold `json:"thresholds"`
+}
+
+// MetaData contains the metadata for a Configuration
+type MetaData struct {
+	Monitoring string `json:"monitoring"`
+	Team       string `json:"string"`
+	Source     string `json:"source"`
+	Targethost string `json:"targethost"`
+}
+
+// Threshold contains the specification for a threshold of
+// a Configuration
+type Threshold struct {
+	Predicate string `json:"predicate"`
+	Level     uint16 `json:"level"`
+	Value     int64  `json:"value"`
+}
+
+// NewConfigurationResult ...
+func NewConfigurationResult() Result {
+	return Result{
+		Errors:         &[]string{},
+		Configurations: &[]Configuration{},
+	}
+}
+
+// vim: ts=4 sw=4 sts=4 noet fenc=utf-8 ffs=unix
---
+++ lib/eye.proto/v2__result.go
@@ -0,0 +1,50 @@
+/*-
+ * Copyright © 2018, 1&1 Internet SE
+ * All rights reserved.
+ *
+ * Use of this source code is governed by a 2-clause BSD license
+ * that can be found in the LICENSE file.
+ */
+
+// Package proto implements the protocol datastructures for eye
+package proto // import "github.com/mjolnir42/eye/lib/eye.proto"
+
+// Numeric result status codes
+const (
+	StatusOK      = 200
+	StatusPartial = 206
+)
+
+// DisplayStatus holds the string representation of the various status
+// codes
+var DisplayStatus = map[int]string{
+	200: "OK",
+	206: "Partial result",
+}
+
+// Result ...
+type Result struct {
+	StatusCode     uint16           `json:"statusCode"`
+	StatusText     string           `json:"statusText"`
+	Errors         *[]string        `json:"errors,omitempty"`
+	Configurations *[]Configuration `json:"configurations,omitempty"`
+}
+
+// OK updates the status fields to indicate a partial result if the
+// result contains one or more error messages, and success otherwise
+func (r *Result) OK() {
+	if r.Errors == nil || *r.Errors == nil || len(*r.Errors) == 0 {
+		r.StatusCode = StatusOK
+		r.StatusText = DisplayStatus[StatusOK]
+		return
+	}
+	r.Partial()
+}
+
+// Partial updates the status fields to indicate a partial result
+func (r *Result) Partial() {
+	r.StatusCode = StatusPartial
+	r.StatusText = DisplayStatus[StatusPartial]
+}
+
+// vim: ts=4 sw=4 sts=4 noet fenc=utf-8 ffs=unix

9cdb6974480bc39341f89a6e71d278a8c43906d9
Pernfuß, Jörg <joerg.pernfuss@1und1.de>
2018-04-17 08:24:50 +0200

    rest: update HTTP dispatch helper

--- internal/eye.rest/util__dispatch.go
+++ internal/eye.rest/util__dispatch.go
@@ -9,27 +9,15 @@
 package rest // import "github.com/mjolnir42/eye/internal/eye.rest"
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
+	"reflect"
 	"runtime/debug"
+
+	"github.com/mjolnir42/soma/lib/proto"
 )
-
-func dispatchForbidden(w *http.ResponseWriter, err error) {
-	if err != nil {
-		http.Error(*w, err.Error(), http.StatusForbidden)
-		return
-	}
-	http.Error(*w, http.StatusText(http.StatusForbidden), http.StatusForbidden)
-}
-
-func dispatchBadRequest(w *http.ResponseWriter, reason string) {
-	if reason != `` {
-		http.Error(*w, reason, http.StatusBadRequest)
-		return
-	}
-	http.Error(*w, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
-}
 
 func panicCatcher(w http.ResponseWriter) {
 	if r := recover(); r != nil {
@@ -40,18 +28,68 @@ func panicCatcher(w http.ResponseWriter) {
 	}
 }
 
-func dispatchJSONReply(w *http.ResponseWriter, b *[]byte) {
-	(*w).Header().Set("Content-Type", "application/json")
-	(*w).WriteHeader(http.StatusOK)
-	(*w).Write(*b)
+// dispatchBadRequest returns a 400 error
+func dispatchBadRequest(w *http.ResponseWriter, reason string) {
+	if reason != `` {
+		http.Error(*w, reason, http.StatusBadRequest)
+		return
+	}
+	http.Error(*w, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
 }
 
+// dispatchForbidden returns a 403 error
+func dispatchForbidden(w *http.ResponseWriter, err error) {
+	if err != nil {
+		http.Error(*w, err.Error(), http.StatusForbidden)
+		return
+	}
+	http.Error(*w, http.StatusText(http.StatusForbidden), http.StatusForbidden)
+}
+
+// dispatchInternalError returns a 500 error
 func dispatchInternalError(w *http.ResponseWriter, err error) {
 	if err != nil {
 		http.Error(*w, err.Error(), http.StatusInternalServerError)
 		return
 	}
 	http.Error(*w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+}
+
+// dispatchBadGateway returns a 502 error
+func dispatchBadGateway(w *http.ResponseWriter, err error) {
+	if err != nil {
+		http.Error(*w, err.Error(), http.StatusBadGateway)
+		return
+	}
+	http.Error(*w, http.StatusText(http.StatusBadGateway), http.StatusBadGateway)
+}
+
+// dispatchGatewayTimeout returns a 504 error
+func dispatchGatewayTimeout(w *http.ResponseWriter, err error) {
+	if err != nil {
+		http.Error(*w, err.Error(), http.StatusGatewayTimeout)
+		return
+	}
+	http.Error(*w, http.StatusText(http.StatusGatewayTimeout), http.StatusGatewayTimeout)
+}
+
+func decodeJSONBody(r *http.Request, s interface{}) (err error) {
+	decoder := json.NewDecoder(r.Body)
+
+	switch s.(type) {
+	case *proto.PushNotification:
+		c := s.(*proto.PushNotification)
+		err = decoder.Decode(c)
+	default:
+		err = fmt.Errorf("decodeJSONBody: unhandled request type: %s", reflect.TypeOf(s))
+	}
+	return
+}
+
+func dispatchJSONReply(w *http.ResponseWriter, b *[]byte) {
+	(*w).Header().Set("Content-Type", "application/json")
+	(*w).WriteHeader(http.StatusOK)
+	(*w).Write(*b)
 }
 
 // vim: ts=4 sw=4 sts=4 noet fenc=utf-8 ffs=unix

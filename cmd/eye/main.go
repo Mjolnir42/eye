85bc2d5dc0a5fbb1d2508723860bceb6daf39a3c
Georg Doser <georg.doser@1und1.de>
2019-03-15 11:19:23 +0100

    Fix linter error / shadowing of err variable

--- cmd/eye/main.go
+++ cmd/eye/main.go
@@ -48,6 +48,7 @@ func main() {
 
 func daemon() int {
 	var err error
+	var f *os.File
 	var configurationFile string
 	var lfhApp *reopen.FileWriter
 
@@ -63,24 +64,24 @@ func daemon() int {
 	cliConfPath := goopt.String([]string{`-c`, `--config`}, `/srv/eye/conf/eye.conf`, `Configuration file`)
 	goopt.Parse(nil)
 	if *cpuprofile != "" {
-		f, err := os.Create(*cpuprofile)
+		f, err = os.Create(*cpuprofile)
 		if err != nil {
 			log.Fatal("could not create CPU profile: ", err)
 		}
 		defer f.Close()
-		if err := pprof.StartCPUProfile(f); err != nil {
+		if err = pprof.StartCPUProfile(f); err != nil {
 			log.Fatal("could not start CPU profile: ", err)
 		}
 		defer pprof.StopCPUProfile()
 	}
 	if *memprofile != "" {
-		f, err := os.Create(*memprofile)
+		f, err = os.Create(*memprofile)
 		if err != nil {
 			log.Fatal("could not create memory profile: ", err)
 		}
 		defer f.Close()
 		rt.GC() // get up-to-date statistics
-		if err := pprof.WriteHeapProfile(f); err != nil {
+		if err = pprof.WriteHeapProfile(f); err != nil {
 			log.Fatal("could not write memory profile: ", err)
 		}
 	}
